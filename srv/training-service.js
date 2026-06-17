b48635adcd186dcb7e2a1b52a2ff60e78c7fc992
ayuwn <ayuwidianingsih@students.unnes.ac.id>
2026-06-17 10:58:40 +0700

    Fix bug v2

--- srv/training-service.js
+++ srv/training-service.js
@@ -1,3 +1,36 @@
+// const cds = require("@sap/cds");
+
+// module.exports = class TrainingService extends cds.ApplicationService {
+//     async init() {
+//         const { TrainingRequest } = this.entities;
+
+//         async function getNextId() {
+//             const active = await SELECT.one`max(TrainingRequestID) as maxID`
+//                 .from(TrainingRequest);
+
+//             const draft = await SELECT.one`max(TrainingRequestID) as maxID`
+//                 .from(TrainingRequest.drafts);
+
+//             const maxActive = active?.maxID || 0;
+//             const maxDraft = draft?.maxID || 0;
+
+//             return Math.max(maxActive, maxDraft) + 1;
+//         }
+
+//         this.before("NEW", TrainingRequest.drafts, async (req) => {
+//             req.data.TrainingRequestID = await getNextId();
+//         });
+
+//         this.before("CREATE", TrainingRequest, async (req) => {
+//             if (!req.data.TrainingRequestID || req.data.TrainingRequestID === 0) {
+//                 req.data.TrainingRequestID = await getNextId();
+//             }
+//         });
+
+//         return super.init();
+//     }
+// };
+
 const cds = require("@sap/cds");
 
 module.exports = class TrainingService extends cds.ApplicationService {
@@ -25,6 +58,34 @@ module.exports = class TrainingService extends cds.ApplicationService {
             if (!req.data.TrainingRequestID || req.data.TrainingRequestID === 0) {
                 req.data.TrainingRequestID = await getNextId();
             }
+        });
+
+        this.on("acceptTrainingRequest", TrainingRequest, async (req) => {
+            const trainingRequestUUID = req.params[0].TrainingRequestUUID;
+
+            await UPDATE(TrainingRequest)
+                .set({
+                    TrainingStatus_code: "A"
+                })
+                .where({
+                    TrainingRequestUUID: trainingRequestUUID
+                });
+
+            return {};
+        });
+
+        this.on("rejectTrainingRequest", TrainingRequest, async (req) => {
+            const trainingRequestUUID = req.params[0].TrainingRequestUUID;
+
+            await UPDATE(TrainingRequest)
+                .set({
+                    TrainingStatus_code: "X"
+                })
+                .where({
+                    TrainingRequestUUID: trainingRequestUUID
+                });
+
+            return {};
         });
 
         return super.init();
